caacdbfc251ef1fa9541118edd653040b4ee973a
Juliano Reis <julianovreis@gmail.com>
2025-05-20 10:19:54 -0300

    update javascript

---
+++ javascript/ex028/script.js
@@ -0,0 +1,20 @@
+const converterInt = (e) => parseInt(e)
+const dobrar = (e) => e * 2
+let num = ['1', '2', '3', '4', '5'].map(dobrar)
+console.log(num)
+
+// let el = document.getElementsByName('div')
+// el = [...el]
+
+// console.log(el)
+
+// el.map((e, i) => {
+// 	e.innerHTML = 'CFB Cursos'
+// })
+
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// let c = cursos.map((el, i) => {
+// 	return el
+// })
+
+// console.log(c)
